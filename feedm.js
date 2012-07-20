c715a43be8dc0db3952cf3f2fc6d26065f036d14
Matt Layman <matthewlayman@gmail.com>
2012-07-20 18:05:28 -0400

    Now rendering a feeding

--- feedm.js
+++ feedm.js
@@ -1,4 +1,6 @@
 $(function() {
+    //TODO: Put all the globals into a namespace like FEEDM.
+
     // The basic unit of measure is a feeding.
     var Feeding = Backbone.Model.extend({});
 
@@ -10,9 +12,37 @@ $(function() {
 
     var FeedingView = Backbone.View.extend({
         tagName: 'li',
+
+        // Cache the template function.
         template: _.template($('#feeding-template').html()),
+
+        // Display the date from the time (in milliseconds).
+        date: function(time) {
+            var date = new Date(time),
+                month;
+            month = date.getMonth() + 1;
+            // XXX: This should be friendlier (like Yesterday, Monday, etc.)
+            return date.getFullYear() + '-' + month + '-' + date.getDate()
+        },
+
+        // Get the readable time from the milliseconds.
+        time: function(ms) {
+            var d = new Date(ms),
+                hour,
+                meridiem;
+            hour = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+            // Midnight should be 12 not 0.
+            hour = hour === 0 ? 12 : hour;
+            meridiem = d.getHours() > 12 ? 'pm' : 'am'
+            return hour + ':' + d.getMinutes() + meridiem;
+        },
+
         render: function() {
-            // TODO: Add li in ul.
+            var model = this.model.toJSON();
+            console.log(model);
+            model.date = this.date(model.time);
+            model.time = this.time(model.time);
+            this.$el.html(this.template(model));
             return this;
         }
     });
@@ -40,13 +70,30 @@ $(function() {
     });
     var feedingsHistory = new FeedingsHistory({collection: feedings});
 
-    //var Router = Backbone.Router.extend({
-    //    routes: {
-    //        '': 'root'
-    //    }
-    //});
-    //var router = new Router;
-    //Backbone.history.start({pushState: true});
+    var Router = Backbone.Router.extend({
+        routes: {
+            'addNow': 'addNow',
+            'addFrom': 'addFrom'
+        },
+
+        addNow: function() {
+            // Show the view for adding something now.
+            feedings.create({
+                time: Date.now(),
+                oz: 4,
+                ml: 125,
+                relativeSize: 'same'
+            });
+            console.log(feedings);
+            alert('Adding now!');
+        },
+
+        addFrom: function() {
+            alert('Adding sometime!');
+        }
+    });
+    var router = new Router;
+    Backbone.history.start();
 
     // Kick things off and fetch the past feedings to display.
     feedings.on('reset', feedingsHistory.render, feedingsHistory);
