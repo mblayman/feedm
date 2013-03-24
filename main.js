fa9b8f903c9f9816d3d8ea5f58ad82d39c82b13a
Matt Layman <matthewlayman@gmail.com>
2013-03-24 19:31:52 -0400

    Linked RequireJS and jQM together.

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 requirejs.config({
     paths: {
-        backbone: 'vendor/backbone',
-        'backbone.localStorage': 'vendor/backbone.localStorage',
-        jquery: 'vendor/jquery',
+        backbone: 'vendor/backbone-1.0.0',
+        'backbone.localStorage': 'vendor/backbone.localStorage-1.1.0',
+        jquery: 'vendor/jquery-1.9.1',
+        'jquery.mobile': 'vendor/jquery.mobile-1.3.0',
+        json2: 'vendor/json2',
         text: 'vendor/text',
-        underscore: 'vendor/underscore'
+        underscore: 'vendor/underscore-1.4.4'
     },
     shim: {
         backbone: {
-            deps: ['jquery', 'underscore'],
+            deps: ['jquery', 'underscore', 'json2'],
             exports: function($) {
                 // Backbone is referencing $ directly in setElement and this is
                 // blowing up during optimization. Therefore, set jQuery.
@@ -20,6 +22,9 @@ requirejs.config({
             deps: ['backbone'],
             exports: 'Backbone.LocalStorage'
         },
+        'jquery.mobile': {
+            deps: ['jquery']
+        },
         underscore: {
             exports: '_'
         }
@@ -27,12 +32,17 @@ requirejs.config({
 });
 
 require([
-    'jquery'
+    'jquery',
+    'jquery.mobile'
     ],
     function($) {
 
     // Views reference DOM elements and should only be instantiated onReady.
     $(function() {
+        // Prevent flash of unstyled content by hiding the body then showing
+        // when jQuery is ready.
+        $('body').show();
+
         // TODO: Finally time to do something.
     });
 });
